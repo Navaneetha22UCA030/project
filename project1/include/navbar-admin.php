4f29a54fb1fa396cad1032e9890d07e7b1f95f2b
Navaneetha Krishnan A <142994603+Navaneetha22UCA030@users.noreply.github.com>
2025-02-22 17:49:19 +0530

    Add files via upload

---
+++ project1/include/navbar-admin.php
@@ -0,0 +1,17 @@
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Sidebar Toggle Button -->
+
+        <a class="navbar-brand ps-3" href="dashboard-admin.php">Admin Panel</a>
+        
+        <!-- User Dropdown -->
+        <ul class="navbar-nav ms-auto me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-user fa-fw"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
